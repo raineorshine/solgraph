431f5b53c67631c00995730eadfd2b9771f6292e
Raine Revere <raineorshine@gmail.com>
2016-07-05 15:21:22 -0600

    Add babel postinstall step.

--- src/solgraph.js
+++ src/solgraph.js
@@ -2,15 +2,15 @@
 
 import com from 'commander'
 import stdin from 'get-stdin-promise'
-import pkg from './package.json'
+import pkg from '../package.json'
 import solgraph from './index.js'
 
 const extendedHelp = `
 
 ${pkg.description}
 
-Here is an example:
-$ `
+Example:
+$ cat MyContract.sol | solgraph > MyContract.dot`
 
 com
   .version(pkg.version)
